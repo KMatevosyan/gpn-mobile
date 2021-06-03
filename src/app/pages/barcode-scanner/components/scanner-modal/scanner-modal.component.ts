523e67fdc05ab968583db625fcc8e996b1815eed
KMatevosyan <71639310+KMatevosyan@users.noreply.github.com>
2021-06-03 20:05:01 +0700

    beta

--- src/app/pages/barcode-scanner/components/scanner-modal/scanner-modal.component.ts
+++ src/app/pages/barcode-scanner/components/scanner-modal/scanner-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
+import {ModalController} from "@ionic/angular";
 
 @Component({
   selector: 'app-scanner-modal',
@@ -7,14 +8,22 @@ import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
   styleUrls: ['./scanner-modal.component.scss'],
 })
 export class ScannerModalComponent implements OnInit {
+    barcodeData: any;
+    constructor(
+        private barcodeScanner: BarcodeScanner,
+        public modalController: ModalController
+    ) { }
 
-  constructor(private barcodeScanner: BarcodeScanner) { }
+    public dismiss(): void {
+        this.modalController.dismiss().then();
+    }
 
-  ngOnInit() {
-      this.barcodeScanner.scan().then(barcodeData => {
-          console.log('Barcode data', barcodeData);
-      }).catch(err => {
-          console.log('Error', err);
-      });
-  }
+    ngOnInit() {
+        this.barcodeScanner.scan().then(barcodeData => {
+            this.barcodeData = barcodeData;
+            console.log('Barcode data', barcodeData);
+        }).catch(err => {
+            console.log('Error', err);
+        });
+    }
 }
